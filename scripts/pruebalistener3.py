c3bac94a8708b10accc377507a955d839ff00dd2
Aaron Arias <aaronarias4b@gmail.com>
2018-03-02 19:55:44 +0100

    for the arms it is working, for the legs it still has bugs

--- scripts/pruebalistener3.py
+++ scripts/pruebalistener3.py
@@ -6,81 +6,109 @@ from skeleton_angles.msg import AngleMessage
 import math
 import time
 
-esqueletos = []
+skeleton = []
 cont_vertices = 0
 rate = 0
 
-#frecuencia de publicacion
+#elementos_cuerpo = {'head_1', 'neck_1', 'torso_1', 'left_shoulder_1', 'left_elbow_1', 'left_hand_1', 'right_shoulder_1', 'right_elbow_1', 'right_hand_1', 'left_hip_1', 'left_knee_1', 'left_foot_1', 'right_hip_1', 'right_knee_1', 'right_foot_1'}
 
 
 def callback(message):
 
-	global esqueletos
+	global skeleton
 	global cont_vertices
 	
 	#rospy.loginfo(rospy.get_caller_id() + '\nParte: %s\n' +'Valores: \n%s', message.transforms[0].child_frame_id, message.transforms[0].transform)		
 	if cont_vertices<15:
 
-		if message.transforms[0].child_frame_id == 'right_elbow_1':
-			aux = message.transforms[0].transform.translation
-			esqueletos.insert(0,aux)
-
-		if message.transforms[0].child_frame_id == 'right_hand_1':
-			aux = message.transforms[0].transform.translation
-			esqueletos.insert(1,aux)
-
-		if message.transforms[0].child_frame_id == 'right_shoulder_1':
-			aux = message.transforms[0].transform.translation
-			esqueletos.insert(2,aux)
-
-		if message.transforms[0].child_frame_id == 'head_1':
-			aux = message.transforms[0].transform.translation
-			esqueletos.insert(3,aux)
-
-		if message.transforms[0].child_frame_id == 'torso_1':
-			aux = message.transforms[0].transform.translation
-			esqueletos.insert(4,aux)
-
-		if message.transforms[0].child_frame_id == 'neck_1':
-			aux = message.transforms[0].transform.translation
-			esqueletos.insert(5,aux)
-
-		if message.transforms[0].child_frame_id == 'right_hip_1':
-			aux = message.transforms[0].transform.translation
-			esqueletos.insert(6,aux)
-
-		if message.transforms[0].child_frame_id == 'right_foot_1':
-			aux = message.transforms[0].transform.translation
-			esqueletos.insert(7,aux)
-
-		if message.transforms[0].child_frame_id == 'right_knee_1':
-			aux = message.transforms[0].transform.translation
-			esqueletos.insert(8,aux)
+		fill_skeleton(message)
 
 		cont_vertices+=1
 	else:
 
 		cont_vertices=0
 
-		calculate_angles()
+		calculate_angles() #this is called only when the skeleton is filled
 
-		esqueletos = []
+		skeleton = []
 
 		#time.sleep(3)
 
+def fill_skeleton(message):
+	if message.transforms[0].child_frame_id == 'head_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(0,aux)
+
+	if message.transforms[0].child_frame_id == 'neck_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(1,aux)
+
+	if message.transforms[0].child_frame_id == 'torso_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(2,aux)	
+
+	if message.transforms[0].child_frame_id == 'left_shoulder_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(3,aux)
+
+	if message.transforms[0].child_frame_id == 'left_elbow_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(4,aux)
+
+	if message.transforms[0].child_frame_id == 'left_hand_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(5,aux)
+
+	if message.transforms[0].child_frame_id == 'right_shoulder_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(6,aux)
+
+	if message.transforms[0].child_frame_id == 'right_elbow_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(7,aux)
+
+	if message.transforms[0].child_frame_id == 'right_hand_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(8,aux)
+
+	if message.transforms[0].child_frame_id == 'right_hip_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(9,aux)
+
+	if message.transforms[0].child_frame_id == 'right_knee_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(10,aux)
+
+	if message.transforms[0].child_frame_id == 'right_foot_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(11,aux)
+
+	if message.transforms[0].child_frame_id == 'left_hip_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(12,aux)
+
+	if message.transforms[0].child_frame_id == 'left_knee_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(13,aux)
+
+	if message.transforms[0].child_frame_id == 'left_foot_1':
+		aux = message.transforms[0].transform.translation
+		skeleton.insert(14,aux)
+	
 		
-def print_punto(vector):
+def print_point(vector):
 	print vector.x
 	print vector.y
 	print vector.z
+
 
 def getAngle(vec1, vec2):
 	mod1 = math.sqrt(pow(vec1.x,2) + pow(vec1.y,2) + pow(vec1.z,2))
 	mod2 = math.sqrt(pow(vec2.x,2) + pow(vec2.y,2) + pow(vec2.z,2))
 
-	producto = vec1.x*vec2.x + vec1.y*vec2.y + vec1.z*vec2.z
+	prod = vec1.x*vec2.x + vec1.y*vec2.y + vec1.z*vec2.z
 
-	angle = math.acos((float(producto)/float(mod1*mod2)))
+	angle = math.acos((float(prod)/float(mod1*mod2)))
 
 	return math.degrees(angle)
 
@@ -89,7 +117,7 @@ def get_points():
 	global pub
 	global rate
 
-	pub = rospy.Publisher('angulos', AngleMessage, queue_size=10)
+	pub = rospy.Publisher('angles', AngleMessage, queue_size=10)
 
 	rospy.init_node('AnglesCalculator', anonymous=True)
 
@@ -104,63 +132,129 @@ def calculate_angles():
 	global rate
 	global pub
 
-	codo_mano = Vector()
-	codo_mano.x = esqueletos[1].x - esqueletos[0].x
-	codo_mano.y = esqueletos[1].y - esqueletos[0].y
-	codo_mano.z = esqueletos[1].z - esqueletos[0].z
+	#for angle 0
+	elbow_hand_l = Vector()
+	elbow_hand_l.x = skeleton[5].x - skeleton[4].x
+	elbow_hand_l.y = skeleton[5].y - skeleton[4].y
+	elbow_hand_l.z = skeleton[5].z - skeleton[4].z
 
-	codo_hombro = Vector()
-	codo_hombro.x = esqueletos[2].x - esqueletos[0].x
-	codo_hombro.y = esqueletos[2].y - esqueletos[0].y
-	codo_hombro.z = esqueletos[2].z - esqueletos[0].z
+	elbow_shoulder_l = Vector()
+	elbow_shoulder_l.x = skeleton[3].x - skeleton[4].x
+	elbow_shoulder_l.y = skeleton[3].y - skeleton[4].y
+	elbow_shoulder_l.z = skeleton[3].z - skeleton[4].z
 
-	cuello_cabeza = Vector()
-	cuello_cabeza.x = esqueletos[3].x - esqueletos[5].x
-	cuello_cabeza.y = esqueletos[3].y - esqueletos[5].y
-	cuello_cabeza.z = esqueletos[3].z - esqueletos[5].z
+	#for angle 1
+	shoulder_elbow_l = Vector()
+	shoulder_elbow_l.x = skeleton[4].x - skeleton[3].x
+	shoulder_elbow_l.y = skeleton[4].y - skeleton[3].y
+	shoulder_elbow_l.z = skeleton[4].z - skeleton[3].z
 
-	cuello_torso = Vector()
-	cuello_torso.x = esqueletos[4].x - esqueletos[5].x
-	cuello_torso.y = esqueletos[4].y - esqueletos[5].y
-	cuello_torso.z = esqueletos[4].z - esqueletos[5].z
+	shoulder_neck_l = Vector()
+	shoulder_neck_l.x = skeleton[1].x - skeleton[3].x
+	shoulder_neck_l.y = skeleton[1].y - skeleton[3].y
+	shoulder_neck_l.z = skeleton[1].z - skeleton[3].z
 
-	rodilla_cadera = Vector()
-	rodilla_cadera.x = esqueletos[6].x - esqueletos[8].x
-	rodilla_cadera.y = esqueletos[6].y - esqueletos[8].y
-	rodilla_cadera.z = esqueletos[6].z - esqueletos[8].z
+	#for angle 2
+	elbow_hand_r = Vector()
+	elbow_hand_r.x = skeleton[8].x - skeleton[7].x
+	elbow_hand_r.y = skeleton[8].y - skeleton[7].y
+	elbow_hand_r.z = skeleton[8].z - skeleton[7].z
 
-	rodilla_pie = Vector()
-	rodilla_pie.x = esqueletos[7].x - esqueletos[8].x
-	rodilla_pie.y = esqueletos[7].y - esqueletos[8].y
-	rodilla_pie.z = esqueletos[7].z - esqueletos[8].z
+	elbow_shoulder_r = Vector()
+	elbow_shoulder_r.x = skeleton[6].x - skeleton[7].x
+	elbow_shoulder_r.y = skeleton[6].y - skeleton[7].y
+	elbow_shoulder_r.z = skeleton[6].z - skeleton[7].z
 
-	#print_punto(codo_mano)
-	#print_punto(codo_hombro)
+	#for angle 3
+	shoulder_elbow_r = Vector()
+	shoulder_elbow_r.x = skeleton[7].x - skeleton[6].x
+	shoulder_elbow_r.y = skeleton[7].y - skeleton[6].y
+	shoulder_elbow_r.z = skeleton[7].z - skeleton[6].z
 
-	ang_codo = getAngle(codo_mano, codo_hombro)
-	ang_cuello = getAngle(cuello_torso, cuello_cabeza)
-	ang_rodilla = getAngle(rodilla_pie, rodilla_cadera)
+	shoulder_neck_r = Vector()
+	shoulder_neck_r.x = skeleton[1].x - skeleton[6].x
+	shoulder_neck_r.y = skeleton[1].y - skeleton[6].y
+	shoulder_neck_r.z = skeleton[1].z - skeleton[6].z
 
-	#mod1 = math.sqrt(pow(codo_mano.x,2) + pow(codo_mano.y,2) + pow(codo_mano.z,2))
-	#mod2 = math.sqrt(pow(codo_hombro.x,2) + pow(codo_hombro.y,2) + pow(codo_hombro.z,2))
+	#for angle 4
+	knee_foot_l = Vector()
+	knee_foot_l.x = skeleton[11].x - skeleton[10].x
+	knee_foot_l.y = skeleton[11].y - skeleton[10].y
+	knee_foot_l.z = skeleton[11].z - skeleton[10].z
 
-	#producto = codo_mano.x*codo_hombro.x + codo_mano.y*codo_hombro.y + codo_mano.z*codo_hombro.z
+	knee_hip_l = Vector()
+	knee_hip_l.x = skeleton[9].x - skeleton[10].x
+	knee_hip_l.y = skeleton[9].y - skeleton[10].y
+	knee_hip_l.z = skeleton[9].z - skeleton[10].z
 
-	#angle = math.acos((float(producto)/float(mod1*mod2)))
+	#for angle 5
+	hip_knee_l = Vector()
+	hip_knee_l.x = skeleton[10].x - skeleton[9].x
+	hip_knee_l.y = skeleton[10].y - skeleton[9].y
+	hip_knee_l.z = skeleton[10].z - skeleton[9].z
 
-	#print '***********************'
-	#print ('Angulo: %s' % math.degrees(angle))
+	hip_torso_l = Vector()
+	hip_torso_l.x = skeleton[2].x - skeleton[9].x
+	hip_torso_l.y = skeleton[2].y - skeleton[9].y
+	hip_torso_l.z = skeleton[2].z - skeleton[9].z
 
-	#print '*********************'
-	#print 'AnguloCodo: %s' % ang_codo
+	#for angle 6
+	knee_foot_r = Vector()
+	knee_foot_r.x = skeleton[14].x - skeleton[13].x
+	knee_foot_r.y = skeleton[14].y - skeleton[13].y
+	knee_foot_r.z = skeleton[14].z - skeleton[13].z
+
+	knee_hip_r = Vector()
+	knee_hip_r.x = skeleton[12].x - skeleton[13].x
+	knee_hip_r.y = skeleton[12].y - skeleton[13].y
+	knee_hip_r.z = skeleton[12].z - skeleton[13].z
+
+	#for angle 7
+	hip_knee_r = Vector()
+	hip_knee_r.x = skeleton[13].x - skeleton[12].x
+	hip_knee_r.y = skeleton[13].y - skeleton[12].y
+	hip_knee_r.z = skeleton[13].z - skeleton[12].z
+
+	hip_torso_r = Vector()
+	hip_torso_r.x = skeleton[2].x - skeleton[12].x
+	hip_torso_r.y = skeleton[2].y - skeleton[12].y
+	hip_torso_r.z = skeleton[2].z - skeleton[12].z
+
+	#print_point(elbow_hand_l)
+
+	#left arm
+	ang0 = getAngle(elbow_hand_l, elbow_shoulder_l)
+	ang1 = getAngle(shoulder_elbow_l, shoulder_neck_l)
+
+	#right arm
+	ang2 = getAngle(elbow_hand_r, elbow_shoulder_r)
+	ang3 = getAngle(shoulder_elbow_r, shoulder_neck_r)
+
+	#left leg
+	ang4 = getAngle(knee_foot_l, knee_hip_l)
+	ang5 = getAngle(hip_knee_l, hip_torso_l)
+
+	#right leg
+	ang6 = getAngle(knee_foot_r, knee_hip_r)
+	ang7 = getAngle(hip_knee_r, hip_torso_r)
+
 
 	print '*********************'
-	print 'AnguloCuello: %s' % ang_rodilla
+	print 'Angulo rodilla: %s' % ang4
+	print 'Angulo cadera: %s' % ang5
 
 	array = []
-	array.append(float(ang_codo))
-	array.append(float(ang_cuello))
-	array.append(float(ang_rodilla))
+	array.append(float(ang0))
+	array.append(float(ang1))
+
+	array.append(float(ang2))
+	array.append(float(ang3))
+
+	array.append(float(ang4))
+	array.append(float(ang5))
+
+	array.append(float(ang6))
+	array.append(float(ang7))
 
 	pub.publish(array)
 	#rate.sleep()
@@ -172,6 +266,7 @@ class Vector:
 		self.x = a
 		self.y = b
 		self.z = c
+
 
 if __name__ == '__main__':
 	try:
